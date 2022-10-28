9fa4057a6d5cf9bf2f1e27a5c0c15fb51dc5213f
forcodedancing <just.haha.it@gmail.com>
2022-10-28 15:50:34 +0800

    add create post

---
+++ x/blog/keeper/msg_server_create_post.go
@@ -0,0 +1,18 @@
+package keeper
+
+import (
+	"context"
+
+	"blog/x/blog/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	var post = types.Post{}
+	id := k.AppendPost(ctx, post)
+
+	return &types.MsgCreatePostResponse{Id: id}, nil
+}
--- x/blog/types/types.go
+++ x/blog/types/types.go
@@ -1,1 +1,7 @@
 package types
+
+const (
+	// Keep track of the index of posts
+	PostKey      = "Post/value/"
+	PostCountKey = "Post/count/"
+)
