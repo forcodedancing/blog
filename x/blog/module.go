ab66618e3e596ca73f3a954b1d978c3c554b0319
forcodedancing <just.haha.it@gmail.com>
2022-10-28 16:22:13 +0800

    add query posts

--- x/blog/module.go
+++ x/blog/module.go
@@ -14,6 +14,7 @@ import (
 	"blog/x/blog/client/cli"
 	"blog/x/blog/keeper"
 	"blog/x/blog/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
